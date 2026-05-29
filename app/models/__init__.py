87936bb09ec411e0f82dffb65edf1078186ebf32
Matheus <matheus-larangeira@hotmail.com>
2026-05-29 11:05:05 -0300

    tabelas categorias e produtos feitas

---
+++ app/models/__init__.py
@@ -0,0 +1,8 @@
+from app.models import categoria
+from app.models import produto
+from app.models import usuarios
+
+# Gerar a migration
+# python -m alembic revision --autogenerate -m "Criar tabelas de categorias e produtos.
+
+# Aplicar a migration
